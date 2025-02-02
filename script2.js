806474602b52d17fde6e3e866b26335c15764955
jitu <jahirul.jitu7@gmail.com>
2025-02-02 20:50:29 +0600

    dasd

--- script2.js
+++ script2.js
@@ -1,6 +1,15 @@
-let forms = document.querySelector("signup-form");
+let forms = document.querySelector(".signup-form");
 
 forms.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(forms.username.value);
+  //   console.log(forms.username.value);
+
+  let FormsData = forms.username.value;
+  let checkValidation = /^[a-zA-Z]{6,12}$/;
+
+  if (checkValidation.test(FormsData)) {
+    console.log("Valid");
+  } else {
+    console.log("Not valid");
+  }
 });
