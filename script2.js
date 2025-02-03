92f79354e2ff9a3ff70491ebe909f9f7ef7c4ddd
jitu <jahirul.jitu7@gmail.com>
2025-02-03 23:15:25 +0600

    dasd

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
 let forms = document.querySelector(".signup-form");
+let wrongMsg1 = document.querySelector(".wrongMsg1");
 
 forms.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,8 +9,10 @@ forms.addEventListener("submit", (e) => {
   let checkValidation = /^[a-zA-Z]{6,12}$/;
 
   if (checkValidation.test(FormsData)) {
-    console.log("Valid");
+    wrongMsg1.textContent = "Valid!!";
+    wrongMsg1.style.color = "green";
   } else {
-    console.log("Not valid");
+    wrongMsg1.textContent = "Not Valid!!";
+    wrongMsg1.style.color = "red";
   }
 });
